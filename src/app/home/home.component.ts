8cac58a6789d15a75feda1a90d07cb91f9189427
Nelson Wandurraga <desarrollo@ramedicas.com>
2019-07-22 17:28:45 -0500

    Mejora en el diseño de la lista, se agrega contenido dinamico

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+	travels: any[] = [
+		{
+			date: 'Monday 10th 2:28 PM',
+			source: 'Houston, TX, 33619',
+			destination: 'Atlanta, GA, 30123',
+			price: 250.00,
+			badge: 1
+		},
+		{
+			date: 'Monday 10th 2:28 PM',
+			source: 'Houston, TX, 33619',
+			destination: 'Atlanta, GA, 30123',
+			price: 270.00,
+			badge: 2
+		},
+		{
+			date: 'Monday 10th 2:28 PM',
+			source: 'Houston, TX, 33619',
+			destination: 'Atlanta, GA, 30123',
+			price: 350.00,
+			badge: 3
+		}
+	];
+	constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
